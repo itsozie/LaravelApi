2b15b641faa57a8df8ed4ef4e6535c178472c333
Ozie <itsozie77@gmail.com>
2022-01-28 20:15:54 +0700

    add destroy productGallery

--- app/Http/Controllers/ProductGalleryController.php
+++ app/Http/Controllers/ProductGalleryController.php
@@ -106,6 +106,9 @@ class ProductGalleryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = ProductGallery::findOrFail($id);
+        $item -> delete();
+
+        return redirect()->route('products-galleries.index');
     }
 }
